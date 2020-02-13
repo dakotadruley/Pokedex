e5bd5b3afaf3b11fe236e3a13dabdd4232d0ffaf
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-02-12 17:47:47 -0800

    added some CSS

--- pokedex/src/PokeItem.js
+++ pokedex/src/PokeItem.js
@@ -7,11 +7,23 @@ export default class PokeItem extends Component {
         return (
             <>
             <li className='poke' key={pokes._id}>
-                <div className='pokeContainer'>
-                    <h2>{pokes.pokemon}</h2>
-                    <p className='type1'>{pokes.type_1}</p>
-                    <p className='type2'>{pokes.type_2}</p>
-                </div>
+                <table className='pokeContainer'>
+                    <tr>
+                        <th>{pokes.pokemon}</th>
+                    </tr>
+                    <tr className='tableRow'>
+                        <td>Height: {pokes.height}</td>
+                        <td>Weight: {pokes.weight}</td>
+                        <td>Type 1: {pokes.type_1}</td>
+                        <td>Type 2: {pokes.type_2}</td>
+                    </tr>
+                    <tr className='tableRow'>
+                        <td>HP: {pokes.hp}</td>
+                        <td>Attack: {pokes.attack}</td>
+                        <td>Defense: {pokes.defense}</td>
+                        <td>Speed: {pokes.speed}</td>
+                    </tr>
+                </table>
                 <div className='imageContainer'>
                     <img className='imageSize'src={pokes.url_image} alt={pokes.pokemon}/>
                 </div>
